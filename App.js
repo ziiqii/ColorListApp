0b54329b9f61ab1197079dcf7bd814c5fff0b682
Ziqi <ziiiqiii@gmail.com>
2022-06-12 17:03:30 +0800

    Added function to randomly generate colors

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import BlockRGB from './components/BlockRGB';
-
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import BlockRGB from "./components/BlockRGB";
+import { FlatList } from "react-native-gesture-handler";
+import { add } from "react-native-reanimated";
 
 function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+    { red: 255, green: 0, blue: 0, id: "0" },
+    { red: 0, green: 255, blue: 0, id: "1" },
+    { red: 0, green: 0, blue: 255, id: "2" },
+  ]);
+
+  function renderItem({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
+  function addColor() {
+    setColorArray([
+      ...colorArray,
+      {
+        red: Math.floor(Math.random() * 256),
+        green: Math.floor(Math.random() * 256),
+        blue: Math.floor(Math.random() * 256),
+        id: `${colorArray.length}`
+      }
+    ])
+  }
+
   return (
     <View style={styles.container}>
-      <BlockRGB red={255} green ={0} blue={0} />
-      <BlockRGB red={0} green ={255} blue={0} />
-      <BlockRGB red={0} green ={0} blue={255} />
+      <TouchableOpacity style={{ height: 40, justifyContent: "center"}} onPress={addColor}>
+        <Text>Add Color</Text>
+      </TouchableOpacity>
+      <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
     </View>
   );
 }
@@ -18,7 +42,7 @@ function HomeScreen() {
 const Stack = createStackNavigator();
 
 export default function App() {
-  return(
+  return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
@@ -30,8 +54,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
