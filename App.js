bb6180de0ea4cea9b4fe7d996ced8b968d53b962
Ziqi <ziiiqiii@gmail.com>
2022-06-12 14:57:19 +0800

    Added RGB colors

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import BlockRGB from './components/BlockRGB';
 
-export default function App() {
+
+function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BlockRGB red={255} green ={0} blue={0} />
+      <BlockRGB red={0} green ={255} blue={0} />
+      <BlockRGB red={0} green ={0} blue={255} />
     </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
